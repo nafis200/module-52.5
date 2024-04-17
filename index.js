bba035d5934fc0c4d3cd318a85ab1af79f7ebc2a
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-17 22:05:54 +0600

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 5000;
 const phones = require('./phones.json');
+
+app.use(cors());
 
 app.get('/',(req,res) =>{
      res.send('hellow this is my first project')
